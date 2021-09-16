c33dce34b39908aa958cf6db11c69b8cefe42e23
Luxolo <dyobiso.luxolo@gmail.com>
2021-09-16 11:38:38 -0700

    first commit

---
+++ src/main/java/com/example/springcicdservices/SpringCiCdServicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcicdservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCiCdServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCiCdServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springcicdservices/SpringCiCdServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcicdservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCiCdServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
